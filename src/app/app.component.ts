42b39e2fd216f892a2cd38215a4c9fc6c58a1318
Statir <devastatir@hotmail.com>
2024-03-28 14:21:42 +0100

    Commit Projet2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
+  clickExercice2() {
+    console.log('clic');
+  }
 }
---
+++ src/app/exercice3/exercice3.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercice3',
+  templateUrl: './exercice3.component.html',
+  styleUrls: ['./exercice3.component.css'],
+})
+export class Exercice3Component {
+  status: boolean = true;
+
+  changeStatus() {
+    this.status = !this.status;
+  }
+}
